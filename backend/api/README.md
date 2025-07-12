be38ae6c6579e2b3fb212bd65dfc5881e841b4f1
x0y14 <ya01ma08@gmail.com>
2025-07-12 23:07:54 +0900

    rm api

--- backend/api/README.md
+++
@@ -1,3 +0,0 @@
-apiサーバ部分。
-
-参考: https://zenn.dev/xronotech/articles/273c0bb59173d8
--- backend/api/app/tests/test_endpoints.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-
-def test_healthcheck():
-    with TestClient(app) as client:
-        res = client.get("/healthcheck")
-        assert res.status_code == 200
