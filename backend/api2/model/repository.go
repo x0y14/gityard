14089a912584a7b9a8ff0ca4ce0d0257148e4a20
x0y14 <ya01ma08@gmail.com>
2025-07-16 23:17:35 +0900

    typo

--- backend/api2/model/repository.go
+++ backend/api2/model/repository.go
@@ -6,7 +6,7 @@ import "time"
 type Repository struct {
 	ID             uint      `gorm:"primaryKey"                                                                      json:"id"`
 	OwnerAccountID *uint     `gorm:"uniqueIndex:uq_idx_repositoris_owner_name,priority:1"                            json:"owner_account_id"` // 所有者削除でNULLになるためポインタ型
-	Name           string    `gorm:"type:varchar(255);not null;uniqueIndex:uq_idx_repositoris_owner_name,priority:2" json:"name"`
+	Name           string    `gorm:"type:varchar(255);not null;uniqueIndex:uq_idx_repositories_owner_name,priority:2" json:"name"`
 	IsPrivate      bool      `gorm:"type:tinyint(1);not null;default:0"                                              json:"is_private"`
 	CreatedAt      time.Time `gorm:"default:current_timestamp(3)"                                                    json:"created_at"`
 	UpdatedAt      time.Time `gorm:"default:current_timestamp(3);onUpdate:current_timestamp(3)"                      json:"updated_at"`
